29f2507daa1343e29d393feac0f3a2c8493eff24
Joan Rodrigues <joanlima2005@gmail.com>
2024-10-01 16:59:57 +0200

    dfd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,36 +8,28 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-
-
-
 class HomeController extends AbstractController
 {
+    public static $enfermeros = array(
+        array("Pepe", "1234wsd"),
+        array("Paco", "patata"),
+        array("Pepita", "firulay"),
+        array("Benito", "austrolopitecus")
+    );
+
     #[Route('/nursebyname/', name: 'app_nurse', methods: ['GET'])]
     public function nursebyname(Request $request): JsonResponse
     {   
-        $enfermeros = array(
-            array("Pepe", "1234wsd"),
-            array("Paco", "patata"),
-            array("Pepita", "firulay"),
-            array("Benito", "austrolopitecus")
-        );
 
-        $length_enfermeros = count($enfermeros);
+        $length_enfermeros = count(self::$enfermeros);
 
         for ($i = 0; $i < $length_enfermeros; $i++) {
-            if ($enfermeros[$i][0] == $request->get("user") && $enfermeros[$i][1] == $request->get("pass")) {
+            if (self::$enfermeros[$i][0] == $request->get("user") && self::$enfermeros[$i][1] == $request->get("pass")) {
 
                 return new JsonResponse(true);
             }
         }
 
         return new JsonResponse(false);
-
-        /* return $this->json([
-            'message' => 'Nurse',
-            'path' => 'src/Controller/HomeController.php',
-        ]); */
     }
 }
